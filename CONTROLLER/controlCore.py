00b02a8890f8e3f2a73cde41a4de0c4b38dbafce
Djaysel Pessôa <djaysel@outlook.com>
2021-11-30 18:46:53 -0300

    Create controlCore.py

---
+++ CONTROLLER/controlCore.py
@@ -0,0 +1,32 @@
+# from pathlib import Path
+# import os
+from DATA.eRD import ObjExtractRotaDados
+
+
+class controlCore:
+    try:
+
+        def control(self, escolha, pegarDirRaiz):
+            self.escolha = escolha
+            self.pegarDirRaiz = pegarDirRaiz
+
+            if(self.escolha == '0'):
+                active = ObjExtractRotaDados.extract(self.escolha, self.pegarDirRaiz)
+                return active
+            if(self.escolha == '1'):
+                active = ObjExtractRotaDados.extract(self.escolha, self.pegarDirRaiz)
+                return active
+            if(self.escolha == '2'):
+                active = ObjExtractRotaDados.extract(self.escolha, self.pegarDirRaiz)
+                return active
+            if(self.escolha == '3'):
+                active = ObjExtractRotaDados.extract(self.escolha, self.pegarDirRaiz)
+                return active
+            if(self.escolha == '4'):
+                active = ObjExtractRotaDados.extract(self.escolha, self.pegarDirRaiz)
+                return active
+    except:
+        print("- ERRO DESCONHECIDO -")
+
+
+ObjControlCore = controlCore()
